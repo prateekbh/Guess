9922926aa17ef9054bd1315c5e985d029c9529df
Prateek <prateek89born@gmail.com>
2017-01-10 21:31:51 +0530

    adding header

---
+++ component/Header/Header.jsx
@@ -0,0 +1,22 @@
+import {h, Component} from 'preact';
+import {Progress} from 'preact-mdl';
+import { connect } from 'preact-redux';
+import './header.css';
+
+export default class Header extends Component {
+	constructor(){
+		super();
+	}
+	render() {
+		return (
+			<header>
+                <div className="settings">
+                </div>
+                <div className="logo">
+                </div>
+                <div className="coins">
+                </div>
+            </header>
+		);
+	}
+}
--- component/Home/Home.jsx
+++ component/Home/Home.jsx
@@ -1,10 +1,11 @@
 import {h, Component} from 'preact';
 import Splash from '../Splash/Splash.jsx';
+import Header from '../Header/Header.jsx';
 export default class Home extends Component {
 	render(){
 		return (
 			<div>
-				<Splash/>
+				<Header/>
 			</div>
 		);
 	}
