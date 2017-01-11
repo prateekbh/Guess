ccb61e686b2585c8e8920672d0cc384d654d4f85
Prateek <prateek89born@gmail.com>
2017-01-11 08:23:33 +0530

    adding svg icon

--- webpack.config.js
+++ webpack.config.js
@@ -37,6 +37,13 @@ module.exports = {
           'style-loader',
           'css-loader!postcss-loader'
         )
+      },
+      {
+          test: /\.(jpe?g|png|gif|svg)(?:\?.*|)$/i,
+          loaders: [
+              'file?hash=sha512&digest=hex&name=[hash].[ext]',
+              'image-webpack'
+          ]
       }
 	  ]
   },
