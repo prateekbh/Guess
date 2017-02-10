7c545eba5958abbeb148b42ad338a004a10be478
Prateek Bhatnagar <prateek89born@gmail.com>
2017-02-10 19:10:30 +0530

    redux logger only on dev

--- scripts/user-store.js
+++ scripts/user-store.js
@@ -15,9 +15,13 @@ const reducer = storage.reducer(combineReducers(reducers));
 const logger = createLogger({
   predicate: (getState, action) => action.type !== LOG_TIME && action.type !== 'REDUX_STORAGE_SAVE'
 });
+
+let middlewares = [thunk, storageMiddleware];
+!process.env.PROD && middlewares.push(logger);
+
 const store = createStore(
   reducer,
-  applyMiddleware(thunk, logger, storageMiddleware)
+  applyMiddleware(...middlewares)
 );
 
 load(store);
