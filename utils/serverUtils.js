c4f40f2a478ccbf6304b3ba4c5a923057c111c3b
Prateek Bhatnagar <prateek89born@gmail.com>
2017-02-10 18:02:42 +0530

    dev only logging

--- utils/serverUtils.js
+++ utils/serverUtils.js
@@ -1,8 +1,19 @@
-function getEnv(){
-    return (process.env.NODE_ENV === 'dev' || !process.env.NODE_ENV) ? 'DEV' : 'PROD';
+function getEnv() {
+    return (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) ? 'DEV' : 'PROD';
 }
 
+function isDev() {
+    return getEnv() === 'DEV';
+}
+
+function log(stuff) {
+    if (isDev) {
+        console.log(stuff);
+    }
+}
 
 module.exports = {
     getEnv,
+    isDev,
+    log
 };
