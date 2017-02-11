72df97856881a432b1a2c98b85fb21bd07c1a914
Prateek Bhatnagar <prateek89born@gmail.com>
2017-02-11 17:42:36 +0530

    change sw

--- sw.js
+++ sw.js
@@ -14,11 +14,11 @@ goog.swlib.cacheRevisionedAssets([
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', () => self.clients.claim());
 
-goog.swlib.router.registerRoute('/', goog.swlib.cacheFirst({
-  cacheName: 'sw-runtime-caching',
+goog.swlib.router.registerRoute('/', goog.swlib.staleWhileRevalidate({
+  cacheName: 'sw-precaching-revisioned',
 }));
-goog.swlib.router.registerRoute('/play', goog.swlib.cacheFirst({
-  cacheName: 'sw-runtime-caching',
+goog.swlib.router.registerRoute('/play', goog.swlib.staleWhileRevalidate({
+  cacheName: 'sw-precaching-revisioned',
 }));
 goog.swlib.router.registerRoute(
   /https:\/\/images.pexels.com\//, goog.swlib.cacheFirst());
